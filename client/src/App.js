412290169e49daaac0bde64b129cfe0887d8c32f
xyzafaq <xyzafaq@gmail.com>
2023-12-09 05:07:03 +0500

    First version

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import ReactPlayer from 'react-player';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [videoSize, setVideoSize] = useState(1);
+  const playerRef = useRef(null);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
+  const handleEnd = () => {
+    setIsPlaying(false);
+  };
+
+  const handleSizeChange = (value) => {
+    setVideoSize(value);
+  };
+
+  const renderControlButtons = () => (
+    <div className="controls">
+      <button className="side_btn" onClick={() => handleSizeChange(videoSize - 0.1)}>-</button>
+      <button className="register_btn" onClick={handlePlay}>Start</button>
+      <button className="side_btn" onClick={() => handleSizeChange(videoSize + 0.1)}>+</button>
     </div>
   );
-}
+
+  return (
+    <div className="app">
+      <div className='home'>
+        {isPlaying ? null : (<><div className="eclipse"></div></>)}
+        <div className="flex" >
+          <ReactPlayer
+            ref={playerRef}
+            url="/video.mp4"
+            playing={isPlaying}
+            width={`calc(100% * ${videoSize})`}
+            height="auto"
+            controls={false}
+            onEnded={handleEnd}
+          />
+        </div>
+        <div className="flex_align" >
+          {isPlaying ? null : renderControlButtons()}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
